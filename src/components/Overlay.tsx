9e525a467cbc2942fff3fcb0395b423099add145
Kirill Novik <kino6052@colorado.edu>
2020-04-11 12:19:42 -0700

    Fix the Blur Effect

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,41 +1,33 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ShouldShowMenuSubject } from "../services/PlayerService";
 import { useSharedState, getVWString } from "../utils/utils";
 import { BandListDrawer } from "./drawers/BandListDrawer";
 import { MainDrawer } from "./drawers/MainDrawer";
 import { SearchScreenDrawer } from "./drawers/SearchScreenDrawer";
 
-const OverlayWrapper = styled.div<{ isBlurred: boolean }>`
-  ${({ isBlurred }) => `
-    width: 100vw; /* 90% of viewport vidth */
-    height: ${(9 / 16) * 100}; /* ratio = 9/16 * 90 = 50.625 */
-    max-height: 100vh;
-    max-width: ${(16 / 9) * 100}vh; /* 16/9 * 90 = 160 */
-    margin: auto;
-    position: absolute;
-    top:0;bottom:0;
-    left:0;right:0;
-
-    color: white;
-    z-index: 1;
-    display: flex;
-    flex-direction: row;
-    overflow: hidden;
-    ${
-      isBlurred &&
-      `
-        backdrop-filter: blur(${() => getVWString(20)});
-        background-color: rgba(0,0,0,0.5);  
-      `
-    }
-  `}
+const OverlayWrapper = styled.div`
+  width: 100vw; /* 90% of viewport vidth */
+  height: ${(9 / 16) * 100}; /* ratio = 9/16 * 90 = 50.625 */
+  max-height: 100vh;
+  max-width: ${(16 / 9) * 100}vh; /* 16/9 * 90 = 160 */
+  margin: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transition: 0.5s;
+  color: white;
+  z-index: 1;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
 `;
 
 export const Overlay: React.SFC = () => {
-  const [isBlurred] = useSharedState(ShouldShowMenuSubject);
   return (
-    <OverlayWrapper isBlurred={isBlurred}>
+    <OverlayWrapper>
       <MainDrawer />
       <BandListDrawer />
       <SearchScreenDrawer />
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
 import styled, { css } from "styled-components";
 import { Overlay } from "./Overlay";
-import { useSharedState } from "../utils/utils";
-import { IsLoadingSubject } from "../services/PlayerService";
+import { useSharedState, getVWString } from "../utils/utils";
+import {
+  IsLoadingSubject,
+  IsPlayingSubject,
+  ShouldShowMenuSubject,
+} from "../services/PlayerService";
 import { Loader } from "./Loader";
 
-const PlayerWrapper = styled.div<{ isLoading: boolean }>`
+const PlayerWrapper = styled.div<{
+  isLoading: boolean;
+  isPaused: boolean;
+  hasMenu: boolean;
+}>`
   width: 100%;
   height: 100vh;
   #player {
@@ -17,19 +25,26 @@ const PlayerWrapper = styled.div<{ isLoading: boolean }>`
     top: 0;
     width: 100%;
     height: 100vh;
-    ${({ isLoading }) =>
-      isLoading &&
+    transition: filter 0.5s;
+    ${({ isLoading, isPaused, hasMenu }) =>
+      (isLoading || isPaused || hasMenu) &&
       css`
-        filter: blur(100px);
-        transform: scale(2);
+        filter: blur(${getVWString(40)}) brightness(0.6);
+        transform: scale(1.1);
       `}
   }
 `;
 
-export const Player: React.SFC = (props) => {
+export const Player: React.SFC = () => {
   const [isLoading] = useSharedState(IsLoadingSubject);
+  const [isPlaying] = useSharedState(IsPlayingSubject);
+  const [hasMenu] = useSharedState(ShouldShowMenuSubject);
   return (
-    <PlayerWrapper isLoading={isLoading}>
+    <PlayerWrapper
+      isLoading={isLoading}
+      isPaused={!isPlaying}
+      hasMenu={hasMenu}
+    >
       <Loader />
       <Overlay />
       <div id="player">Player One</div>
